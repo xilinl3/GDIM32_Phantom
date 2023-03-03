1f6f0f35e534c551ef00dce2313b65c4ab17eb80
xilinl3 <xilinl3@uci.edu>
2023-03-02 23:24:35 -0800

    error

--- Assets/Scripts/Animals/Cow.cs
+++ Assets/Scripts/Animals/Cow.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 //by Xinlin Li
 public class Cow : AnimalInstance
 {
+    public SOAnimalDefinition cow;
+    private string cowname;
+    private int cowvalue;
+    private string cowtype;
+    private Sprite cowicon;
+    private int cowadultGrwothValue;
+    private List<FoodType> cowpreferedFood;
+    private int coweight;
     // Start is called before the first frame update
+    public void getset()
+    {
+        cowname = cow.GetName();
+        cowvalue = cow.GetSellValue();
+        cowtype = "cow";
+        cowicon = cow.GetIcon();
+        cowadultGrwothValue = cow.GetAdultGrowthValue();
+        cowpreferedFood = cow.GetPreferedFood();
+        coweight = cow.GetWeight();
+    }
     void Start()
     {
-        
+        getset();
+        print(cowname);
+        print(cowvalue);
+        print(cowtype);
+        print(cowadultGrwothValue);
+        print(coweight);
+        for (int i = 0; i < cowpreferedFood.Count; i++)
+        {
+            print(cowpreferedFood[i]);
+        }
+
     }
 
     // Update is called once per frame
@@ -15,4 +43,5 @@ public class Cow : AnimalInstance
     {
         
     }
+    
 }
